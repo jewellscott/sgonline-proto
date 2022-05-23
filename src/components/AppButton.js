070512e35aa23ddae715a3c8f0ccf8e4781d66bd
Jewell Scott <jewelljrscott@gmail.com>
2022-05-23 14:23:52 -0400

    Add Basics

---
+++ src/components/AppButton.js
@@ -0,0 +1,13 @@
+const AppButton = ({ label }) => {
+  return (
+    <button 
+      className="btn app-button"
+      // onClick={onClick}
+      // onDoubleClick={onDoubleClick}
+    >
+      {label}
+    </button>
+  )
+}
+
+export default AppButton
--- src/pages/SimGenerator.js
+++ src/pages/SimGenerator.js
@@ -2,18 +2,9 @@
 import Config from '../config.json'
 import Button from "../components/Button";
 import { ReactComponent as SquareLogo } from "../assets/square-logo-tbd.svg";
+import AppButton from '../components/AppButton'
 
 const SimGenerator = () => {
-
-  // const [sims, setSims] = useState([]);
-
-  // const fetchSims = async () => {
-  //   const res = await fetch('http://localhost:5000/packs');
-  //   const json = await res.json();
-
-  //   console.log(json);
-  //   return json;
-  // }
 
   const cas = Config.packs[0].data[0].cas;
   const basics = Config.packs[0].data[0].cas.basics[0];
@@ -23,11 +14,16 @@ const SimGenerator = () => {
   //   return parseInt(Math.random() * array.length);
   // }
 
-  let randomize = (arr) => {
-    arr.sort(function(a, b) {
-      return 0.5 - Math.random()
-    })
-  };
+
+  // let randomize = (arr) => {
+  //   arr.sort(function(a, b) {
+  //     return 0.5 - Math.random()
+  //   })
+  // };
+
+  let randomize = (a, b) => {
+    return Math.random() - 0.5;
+  }
 
   return (
     <main className="tool-main container-1140">        
@@ -51,13 +47,24 @@ const SimGenerator = () => {
         </nav>
         </aside>
         <section className="tool-main-output">
-          <h2>Basics</h2>
+          <h2 className="app-h2">Basics</h2>
           <p>
-            Sul Sul! My initials are { basics.initial[0] }
+            Sul sul! My initials are <AppButton label={ basics.initial.sort(randomize)[0] } /><AppButton label={ basics.initial.sort(randomize)[0] } /> and I live in <AppButton label={ gameplay.worlds.sort(randomize)[0] } />!
           </p>
-          <h2>Appearance</h2>
+          <br/>
+          <p>
+            I work at { gameplay.careers.sort(randomize)[0].workplace } as a <AppButton label={ gameplay.careers[0].branches.sort(randomize)[0] } /> in the <AppButton label={ gameplay.careers[0].name } /> career. 
+            
+            In my free time, I enjoy <AppButton label={ gameplay.preferences.acivities.sort(randomize)[0] } /> and <AppButton label={ gameplay.preferences.acivities[1] } />, but I dislike <AppButton label={ gameplay.preferences.acivities[2] } />. 
+            
+            My friends say I display <AppButton label={ basics.traits.sort(randomize)[0] } />, <AppButton label={ basics.traits[1] } />, and <AppButton label={ basics.traits[3] } /> traits. 
+            
+            My aspiration in life is all about <AppButton label= { basics.aspirations[0].category } />– <AppButton label={ basics.aspirations.sort(randomize)[0].name } />! { basics.aspirations[0].description }!
+
+          </p>
+          <h2 className="app-h2">Appearance</h2>
           <p>TBA</p>
-          <h2>Favorites</h2>
+          <h2 className="app-h2">Favorites</h2>
           <p>TBA</p>
         </section>
       </div>
